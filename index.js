315d855e2c6ce8f25be797c4dcbe264f4b75cc22
UzomaAkpala <francisakpala@yahoo.com>
2023-11-24 14:08:32 +0100

    reading and writing file on nodejs

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const fs = require("fs");
 
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
+
+const textOutput = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+
+fs.writeFileSync("./txt/output.txt", textOutput);
+
+console.log("File written");
